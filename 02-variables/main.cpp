7467c29aeeafc3fc39330fafae111d839f0e611f
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-12 21:25:07 +0600

    variables in c plus plus

---
+++ 02-variables/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"============================================================== variables in C++ =============================================================="<<endl;
+	// What is variable ?
+	// Ans: variable is container where store data is stored inside the memory. For example: if you want to store milk you have to store milk it into a 🥛.
+	int age = 10; // here 10 is stored in age. age is a variable
+	cout << age << endl;
+	return 0;
+}
