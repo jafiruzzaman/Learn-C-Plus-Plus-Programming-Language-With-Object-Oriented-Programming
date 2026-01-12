f5159b4603f2892e773e0033327c885c6fa2bce1
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-12 21:16:17 +0600

    breakdown the c++ program structure

---
+++ 01.01-cpp-program-structure/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"============================================================== C++ Program Structure =============================================================="<<endl;
+	return 0;
+}
+
+/*
+	============================================================== Program Structure ==============================================================
+	#include<iostream> --> it's a header file
+	#include --> it's a preprossor directive
+	<iostream> --> i/o stream library
+	using namespace std; --> std is a default namespace.namespace are isolated.it allows to avoid write everytime std::cout and std::cin
+	int main(){ --> it's a function
+		cout<<"============================================================== C++ Program Structure =============================================================="<<endl; --> print in console
+	return 0; --> it means the function return 0
+	}
+*/
+
+// This is single line comment
+
+/*
+	this
+	is 
+	multi
+	line
+	comment
+*/
