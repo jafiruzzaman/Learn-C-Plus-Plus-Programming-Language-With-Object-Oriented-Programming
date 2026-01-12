55ce6ef15704e7a67781a8c1679cc43907b3d0f2
Mohammad Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-01-12 22:00:53 +0600

    learn about taking input from the user

---
+++ 03.01-user-inputs/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout << "==================================================================== User Input in C++ ====================================================================" << endl;
+	cout<<"Enter your age: ";
+	int age;
+	cin>>age;
+	cout<<"Your age is "<< age <<endl;
+	return 0;
+}
